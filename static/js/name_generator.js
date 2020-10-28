85b96a177fe397048d2a4c4ef2577428c44797a0
diamondwyrm <kent.j.hazen@biola.edu>
2020-10-27 21:23:58 -0700

    edited and played around with the Javascript, with not much success.

---
+++ static/js/name_generator.js
@@ -0,0 +1,2 @@
+var name_area = d3.select(".name-area").append("svg");
+name_area.append("circle").attr("cx","50").attr("cy","50").attr("r","40").style("fill", "teal");
---
+++ static/js/ttt_data.js
@@ -0,0 +1,3 @@
+d3.csv("ttt_game_data.csv", function(data) {
+    console.log(data[0]);
+});
